758058a60514c26c3f02baa9b0203384c7a7ee3c
DiamondYuan <admin@diamondyuan.com>
2019-09-01 14:16:07 +0800

    fix: plugin could be AsyncFunction

--- src/utils.ts
+++ src/utils.ts
@@ -2,7 +2,10 @@ export function codeCallWithContext(plugin?: Function): undefined | string {
   if (!plugin) {
     return;
   }
-  if (Object.prototype.toString.call(plugin) !== '[object Function]') {
+  if (
+    Object.prototype.toString.call(plugin) !== '[object Function]' &&
+    Object.prototype.toString.call(plugin) !== '[object AsyncFunction]'
+  ) {
     throw new TypeError('plugin must be function.');
   }
   return `(${plugin.toString()})(typeof(context) === 'undefined' ? {}:context);`;
