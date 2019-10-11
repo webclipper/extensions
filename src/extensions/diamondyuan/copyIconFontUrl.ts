7e08871aa3f04d37d49b479276bc829b3d8b613d
DiamondYuan <admin@diamondyuan.com>
2019-10-11 19:57:43 +0800

    fix: change manifest of copyIconFontUrl

--- src/extensions/diamondyuan/copyIconFontUrl.ts
+++ src/extensions/diamondyuan/copyIconFontUrl.ts
@@ -5,8 +5,14 @@ export default new ToolExtension(
     name: 'Copy Iconfont Url',
     icon: 'iconfont',
     version: '0.0.1',
-    description: 'Copy Iconfont Url',
+    description: 'Copy Iconfont Url in project page.',
     matches: ['https://www.iconfont.cn/manage/index'],
+    i18nManifest: {
+      'zh-CN': {
+        name: '复制 Iconfont 代码链接',
+        description: '在 Iconfont 项目页，复制当前项目的代码链接。',
+      },
+    },
   },
   {
     run: () => {
