6d2f39050c6225ce2c92e984920122cb83b4c8b3
DiamondYuan <admin@diamondyuan.com>
2019-12-07 22:44:09 +0800

    :sparkles: add new extension

---
+++ src/extensions/web-clipper/download.ts
@@ -0,0 +1,26 @@
+import { ToolExtension } from '../../index';
+
+export default new ToolExtension(
+  {
+    name: 'Save as Markdown',
+    icon: 'file-markdown',
+    version: '0.0.1',
+    description: 'Save as Markdown and Download.',
+    apiVersion: '1.12.0',
+    i18nManifest: {
+      'zh-CN': {
+        name: '保存为 Markdown',
+        description: '保存为 Markdown 并下载',
+      },
+    },
+  },
+  {
+    run: ({ document }) => {
+      return document.title;
+    },
+    afterRun: ({ createAndDownloadFile, data, result }) => {
+      createAndDownloadFile(`${result || 'content'}.md`, data);
+      return data;
+    },
+  }
+);
